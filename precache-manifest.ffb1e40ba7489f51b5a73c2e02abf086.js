e407ad2ab107f5b65aba4c303da54a111056fd23
sasha.kalinskiy@yandex.ru <jsn364cmd025>
2020-09-07 00:18:45 +0300

    Updates

---
+++ precache-manifest.ffb1e40ba7489f51b5a73c2e02abf086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef602078317d96f0f1608a20f3eb61fb",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "dc35c9865ed949560d71",
+    "url": "/homework/static/css/main.5e67bd6f.chunk.css"
+  },
+  {
+    "revision": "39825ff7aeeae1b0b16f",
+    "url": "/homework/static/js/2.a7437a94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homework/static/js/2.a7437a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc35c9865ed949560d71",
+    "url": "/homework/static/js/main.18f8f418.chunk.js"
+  },
+  {
+    "revision": "c15b06a6cb9be4abad51",
+    "url": "/homework/static/js/runtime-main.1deadf5f.js"
+  },
+  {
+    "revision": "3a5c45ae2b11ee122588f7065c42c9dd",
+    "url": "/homework/static/media/man.3a5c45ae.png"
+  }
+]);
